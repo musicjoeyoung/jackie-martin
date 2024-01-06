f0c621d19b26782610bec382a1b5855fa7bc5430
Joe Young <musicjoeyoung@gmail.com>
2024-01-06 12:01:03 -0500

    initial commit

---
+++ src/prelaunch/ComingSoon/ComingSoon.jsx
@@ -0,0 +1,14 @@
+import CountdownTimer from "../CountdownTimer/CountdownTimer"
+import "./ComingSoon.scss"
+
+
+const ComingSoon = () => {
+    return (
+        <div><h1>Coming Soon!</h1>
+            <p>Jackie's website is under construction. Stay tuned!</p>
+            <CountdownTimer />
+        </div>
+    )
+}
+
+export default ComingSoon
