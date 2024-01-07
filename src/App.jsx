773310c4f8f13e8eea48e1a588d0081dab01fdec
Joe Young <musicjoeyoung@gmail.com>
2024-01-07 08:50:50 -0500

    styles; console

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import './App.scss'
 import ComingSoon from './prelaunch/ComingSoon/ComingSoon'
 
 function App() {
+  console.log(
+    '%c🌐 Want a stunning website? 🚀 %cReach out to%c Joe Young %cjoseph.m.young2@gmail.com',
+    'color: #51355a; font-size: 18px; font-weight: bold; text-shadow: 2px 2px 4px rgba(81, 53, 90, 0.5);',
+    'color: #3498db; font-size: 18px; font-weight: bold; text-decoration: underline;',
+    'color: #e74c3c; font-size: 18px; font-weight: bold;',
+    'color: #e67e22; font-size: 18px; font-weight: bold;'
+  );
+
+
 
   return (
     <>
