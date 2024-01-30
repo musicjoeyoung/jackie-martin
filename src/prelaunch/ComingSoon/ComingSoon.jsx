cc01733276edb239109648ad6938fa9bcd1ee96b
Joe Young <musicjoeyoung@gmail.com>
2024-01-30 15:04:12 -0500

    resume add

--- src/prelaunch/ComingSoon/ComingSoon.jsx
+++ src/prelaunch/ComingSoon/ComingSoon.jsx
@@ -1,5 +1,6 @@
 import CountdownTimer from "../CountdownTimer/CountdownTimer"
 import "./ComingSoon.scss"
+import resume from "../../assets/documents/Jackie-Martin-Resume.pdf"
 
 
 const ComingSoon = () => {
@@ -7,6 +8,7 @@ const ComingSoon = () => {
         <div className="coming-soon">
             <h1 className="coming-soon__h1">Coming Soon!</h1>
             <p>Jackie's website is under construction. Stay tuned!</p>
+            <p>While you're here, why not take a look at her <a href={resume} target="_blank" download>   resume</a>!</p>
             <CountdownTimer />
         </div>
     )
